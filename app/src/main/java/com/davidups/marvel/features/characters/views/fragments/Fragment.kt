f0c0d2b7c8632485e8171922679e0a20b5b92fb2
DavidUps <darribas_talentomobile@bankinter.com>
2021-06-02 18:33:11 +0200

    init

--- app/src/main/java/com/davidups/marvel/features/characters/views/fragments/Fragment.kt
+++ app/src/main/java/com/davidups/marvel/features/characters/views/fragments/Fragment.kt
@@ -3,6 +3,7 @@ package com.davidups.marvel.features.characters.views.fragments
 import android.os.Bundle
 import com.davidups.marvel.R
 import com.davidups.marvel.core.platform.BaseFragment
+import com.davidups.marvel.core.platform.viewBinding.viewBinding
 import com.davidups.marvel.databinding.FragmentStartBinding
 
 class Fragment : BaseFragment(R.layout.fragment_start) {
